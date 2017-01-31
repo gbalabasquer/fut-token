6430b3202a6022ab0b1e74581c85224dc6c2c4ef
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2017-01-30 22:14:19 -0300

    Deploy contract in Ropsten

---
+++ src/deploy_fut.ds.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.8;
+
+import "dapple/script.sol";
+import "./fut.sol";
+
+contract DeployFUTToken is Script {
+  function DeployFUTToken () {
+    exportObject("fut", new FUTToken(1000000 * 10**18));
+  }
+}
